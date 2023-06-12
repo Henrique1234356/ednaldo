79278d177342cf0b2fbee11f4c793bcd6b082ae8
Henrique1234356 <127159529+Henrique1234356@users.noreply.github.com>
2023-06-12 20:21:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+var canvas = new fabric.Canvas("myCanvas");
+// Create canvas variable
+ blockY=1;
+ blockX=1;
+
+blockImageWidth = 350;
+blockImageHeight = 430;
+
+var blockImageObject= "";
+
+function newImage(getImage)
+{
+	fabric.Image.fromURL(getImage, function(Img){
+		blockImageObject = Img;
+		blockImageObject.scaleToWidth(blockImageWidth);
+		blockImageObject.scaleToHeight(blockImageHeight);
+		blockImageObject.set({
+			top: blockY,
+			left: blockX
+		
+		});
+		canvas.add(blockImageObject);
+			});
+}
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '69') // adicione os códigos adequados às teclas
+	{
+		newImage('rr1.png');
+	}
+	if(keyPressed == '86')
+	{
+		blockX = 200;
+		newImage('gr.png');// enviar ranger verde
+	}
+	
+	if(keyPressed == '65')
+	{
+		blockX =350;
+		newImage('yr.png');// enviar ranger amarelo
+	}
+	if(keyPressed == '82')
+	{
+		blockX = 600;
+		newImage('pr.png');// enviar ranger rosa
+	}
+	if(keyPressed == '73')
+	{
+		blockX = 700;
+		newImage('br.png');// enviar ranger índigo
+	}
+	
+}
+
